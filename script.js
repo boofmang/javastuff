c74e6e62a1046384743daf111b13f652bfe7efa0
boofmang <topmcveagh@gmail.com>
2024-11-28 12:00:22 +0000

    The 4 basic java function exercises

---
+++ script.js
@@ -0,0 +1,25 @@
+function add7(num1) {
+let result = num1 + 7
+return result
+}
+
+
+add7 (5)
+
+
+function multiply(Num1, Num2) {
+let result = Num1*Num2
+return result
+}
+
+
+
+function capitalize(string){
+const string2 = string.charAt(0).toUpperCase() + string.slice(1)
+return string2
+}
+
+function Lastletter(string){
+    const string2 = string.slice(-1)
+    return string2
+}
